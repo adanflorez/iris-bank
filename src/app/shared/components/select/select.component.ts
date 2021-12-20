e6f8a9df6ba602ef4665e7c8eb18d80040f353ed
Adan Florez <adan.florezb@gmail.com>
2021-12-20 17:24:31 -0500

    feat: custom select component

---
+++ src/app/shared/components/select/select.component.ts
@@ -0,0 +1,36 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import CustomSelect from 'src/app/core/models/custom-select';
+
+type Size = 'sm' | 'lg' | undefined;
+
+@Component({
+  selector: 'app-select',
+  templateUrl: './select.component.html',
+  styleUrls: ['./select.component.scss'],
+})
+export class SelectComponent {
+  // Inputs
+  @Input() options: CustomSelect[] = [];
+  @Input() size: Size;
+  // Output
+  @Output() changeValue = new EventEmitter<string>();
+
+  /**
+   * Returns class for select size
+   *
+   * @returns {string} form-select-{size}
+   */
+  get classes(): string {
+    return this.size ? `form-select-${this.size}` : '';
+  }
+
+  /**
+   * Emit option selected to parent
+   *
+   * @param {Event} event
+   */
+  onChange(event: Event) {
+    const value = (<HTMLSelectElement>event.target).value;
+    this.changeValue.emit(value);
+  }
+}
