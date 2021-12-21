0519ac4abb2ad26299992d7960515ed477f82a8a
Adan Florez <adan.florezb@gmail.com>
2021-12-21 02:56:24 -0500

    feat: ngrx first configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,22 @@ import { AppComponent } from './app.component';
 // Main layout components
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 import { MainLayoutHeaderComponent } from './layouts/main-layout/main-layout-header/main-layout-header.component';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainLayoutComponent,
-    MainLayoutHeaderComponent
-  ],
+  declarations: [AppComponent, MainLayoutComponent, MainLayoutHeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
