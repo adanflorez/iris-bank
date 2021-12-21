27d56921ddc914368d1a8062adbfeae8d214ad68
Adan Florez <adan.florezb@gmail.com>
2021-12-21 01:35:45 -0500

    refactor: to do list improvement

--- src/app/shared/components/todo/todo-list.component.ts
+++ src/app/shared/components/todo/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import ToDo from 'src/app/core/models/todo.interface';
 
 @Component({
@@ -7,22 +7,10 @@ import ToDo from 'src/app/core/models/todo.interface';
   styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent implements OnInit {
-  todoList: ToDo[] = [];
+  @Input() todoList: ToDo[] = [];
 
   constructor() {}
 
   ngOnInit(): void {
-    this.todoList = [
-      {
-        id: '1',
-        text: 'Primera tarea sadbasgdiauhsdioashdoaisdhas da a sdaos a daodaoisdjausdasd ausd asdasud a90sdfsdfsdfsdfsd sdf sfsfewerfwerfwsef...',
-        checked: false,
-      },
-      {
-        id: '2',
-        text: 'Segunda tarea sadbasgdiauhsdioashdoaisdhas da a sdaos a daodaoisdjausdasd ausd asdasud a90sdfsdfsdfsdfsd sdf sfsfewerfwerfwsef...',
-        checked: true,
-      },
-    ];
   }
 }
