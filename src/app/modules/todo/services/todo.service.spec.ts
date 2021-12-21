709793f379351eb91530bed02954f2bf30fe34e9
Adan Florez <adan.florezb@gmail.com>
2021-12-21 05:59:52 -0500

    feat: unit testing for services

--- src/app/modules/todo/services/todo.service.spec.ts
+++ src/app/modules/todo/services/todo.service.spec.ts
@@ -1,16 +1,73 @@
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import ToDo from 'src/app/core/models/todo.interface';
 
 import { TodoService } from './todo.service';
 
 describe('TodoService', () => {
   let service: TodoService;
+  let httpClientSpy: {
+    post: jasmine.Spy;
+    get: jasmine.Spy;
+    delete: jasmine.Spy;
+  };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(TodoService);
+    httpClientSpy = jasmine.createSpyObj('HttpClient', [
+      'post',
+      'get',
+      'delete',
+    ]);
+    service = new TodoService(httpClientSpy as any);
   });
 
-  it('should be created', () => {
+  it('should be created service', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('Get todo list from fake db', (done: DoneFn) => {
+    const item: ToDo = {
+      id: '1',
+      text: 'Prueba',
+      checked: false,
+    };
+
+    const result = [item];
+    httpClientSpy.get.and.returnValue(of(result));
+
+    service.getDataAPI().subscribe({
+      next: (res) => {
+        expect(res).toEqual(result);
+        done();
+      },
+    });
+  });
+
+  it('Add item to fake db', (done: DoneFn) => {
+    const item: ToDo = {
+      id: '1',
+      text: 'Prueba',
+      checked: false,
+    };
+
+    httpClientSpy.post.and.returnValue(of(item));
+
+    service.addItem(item).subscribe({
+      next: (res) => {
+        expect(res).toEqual(item);
+        done();
+      },
+    });
+  });
+
+  it('Remove item from fake db', (done: DoneFn) => {
+    httpClientSpy.delete.and.returnValue(of({}));
+
+    service.removeItem("1").subscribe({
+      next: (res) => {
+        expect(res).toEqual({});
+        done();
+      },
+    });
   });
 });
